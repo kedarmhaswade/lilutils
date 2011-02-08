0d923c9579aaa60ad1654ce26648e1130dfc3cbe
Kedar Mhaswade <kedar.mhaswade@gmail.com>
2011-02-08 10:24:29 -0800

    hooked up the tests to the test_lilutils.rb. Perhaps I need to do better?

--- test/test_lilutils.rb
+++ test/test_lilutils.rb
@@ -1,6 +1,7 @@
 require File.dirname(__FILE__) + '/test_helper'
 require File.dirname(__FILE__) + '/cli/cli_basic_test'
 require File.dirname(__FILE__) + '/algo_test_utils/data_sets_test'
+require File.dirname(__FILE__) + '/misc/misc_pascal_test'
 
 class TestLilutils < Test::Unit::TestCase
 
