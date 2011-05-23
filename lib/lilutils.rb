e7f551f2ffc611f2f9af2d94f842b4141e686127
Kedar Mhaswade <kedar.mhaswade@gmail.com>
2011-05-23 04:57:17 -0700

    fixed a bug in 0.0.2, releasing 0.0.3

--- lib/lilutils.rb
+++ lib/lilutils.rb
@@ -3,6 +3,6 @@ $:.unshift(File.dirname(__FILE__)) unless
 require 'lilutils/cli_entry'
 require 'lilutils/misc_entry'
 require 'lilutils/algo_test_utils_entry'
-module Lilutils
+module LilUtils
   VERSION = '0.0.1'
 end
